585a3db97cdcb8ebbd82246add3b6c643916d287
unknown <maquinamauro@hotmail.com>
2019-11-06 09:55:14 -0300

    Ejercicios extra completos y correccion de ejercicio 3

--- maurodan/js/ejercicio-extra-03.js
+++ maurodan/js/ejercicio-extra-03.js
@@ -1,7 +1,14 @@
+var movimiento = {
+    fecha: "04/05/2019",
+    monto: 3500,
+    numeroDeCuenta: "32123",
+};
+
 var cuentaBancaria = {
     titular: "Jose",
     numerodecuenta: "3213",
 }
 
-cuentaBancaria.movimientos = ["Ingreso", "Extraccion", "Fondos"];
+cuentaBancaria.movimientos = [movimiento];
+
 console.log(cuentaBancaria);
