f606f0fd74666d0b605903d845e08bef53d8659a
unknown <maquinamauro@hotmail.com>
2019-11-06 08:55:07 -0300

    cambios

--- maurodan/js/ejercicio-extra-03.js
+++ maurodan/js/ejercicio-extra-03.js
@@ -1,11 +1,7 @@
 var cuentaBancaria = {
     titular: "Jose",
     numerodecuenta: "3213",
-    function Movimientos(ingreso, retiro, fondo) {
-        this.ingreso = ingreso;
-        this.retiro = retiro;
-        this.fondo = fondo;
-    }
+    
 }
 
 console.log(cuentaBancaria);
