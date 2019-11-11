b304a9ec0e122db2891c307b0d04f9effe632c29
fed-tuc <fedban@gmail.com>
2019-11-11 09:37:25 -0300

    ejercicio

--- Fede/ejercicio.js
+++ Fede/ejercicio.js
@@ -8,3 +8,44 @@ var persona = {
     }
 
 console.log (persona.dni)
+
+var products = [
+    { id: 1, name: "Leche", price: 120, categories: ["familiar", "comida"] },
+    { id: 2, name: "Arroz", price: 80, categories: ["familiar", "comida"] },
+    { id: 3, name: "Lavadora", price: 7800, categories: ["electrodomésticos"] }
+  ];
+  
+  for (var i=0; i < products.length; i++) {
+    var product = products[i];
+    console.log(product.name);
+    console.log("  Id: " + product.id);
+    console.log("  Precio: " + product.price);
+    console.log("  Categorías: " + product.categories.join(", "));
+  }
+
+    var clase = function () {
+        this.name = "paulo";
+        //  metodo
+        this.setName = function (nombre) {
+            this.name = nombre;
+        }
+        this.getName = function () {
+            return this.name;
+            
+        }
+        
+    }
+
+    // instaciar objetos
+var obj1 = new clase ();
+var obj2 = new clase();
+
+obj2.setName ("cesar");
+
+document.write (obj1.getName()+ '<br>')
+document.write (obj2.getName())
+
+
+
+
+
