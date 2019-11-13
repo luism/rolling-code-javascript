a96353fa7a83240b65d2f4d142697f87f8d9e0fa
Mary Bosch <marybosch.99@gmail.com>
2019-11-13 09:34:56 -0300

    modificaciones

--- Mary/ejercisiosparte1.js
+++ Mary/ejercisiosparte1.js
@@ -40,8 +40,3 @@ if( nota <= 2 ) {
 }
 console.log("Fin del programa.")
 
-/*Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.*/
-
-
-
-
