a40a5ee579aefa21ff86a0d995a67d9bd75fa3e8
Ricardo Luis Mender <rlmender@gmail.com>
2019-11-13 14:34:54 -0300

    Agrego ejemplos con dom y bom. - en el archivo ejemplo-20.js hacemos un contador usando dom y bom.

--- ejericios-extras7.js
+++ ejericios-extras7.js
@@ -4,28 +4,48 @@ class Movimiento {
         this.monto = monto;
         this.fecha = fecha;
     }
+}
 
-    saldo(){
-        
+class CuentaBancaria {
+    constructor(numeroDeCuenta, titular, movimientos) {
+        this.numeroDeCuenta = numeroDeCuenta;
+        this.titular = titular;
+        this.movimientos = movimientos;
+    }
+
+    saldo() {
+        // Recorrer el array y sumar todos sus elementos.
+    }
+
+    otroMetodo(var1) {
+        console.log("Se ejecuto el metodo `otroMetodo`");
+        console.log("var1 es:", var1);
+        return true;
+    }
+
+    metodoSinRetorno() {
+        console.log("Se ejecuto `metodoSinRetorno`")
+    }
+
+    set setNumeroDeCuenta(numero){
+        this.numeroDeCuenta = numero;
+    }
+
+    get getNumeroDeCuenta(){
+        return this.numeroDeCuenta;
     }
 }
 
 // Harcodeamos (Significa que este codigo esta fijo y no es dinamico)
-var movimiento1 = {
-    fecha: "10/11/2019",
-    monto: 5000,
-    numeroDeCuenta: "0000123"
-}
+var movimiento1 = new Movimiento("0000123", 5000, "10/11/2019")
+var movimiento2 = new Movimiento("0000123", -1500, "11/11/2019")
+// Con Date.now() obtenemos la fecha actual automaticamente
+var movimiento3 = new Movimiento("0000123", 500, Date.now())
 
-var movimient2 = {
-    fecha: "11/11/2019",
-    monto: -1500,
-    numeroDeCuenta: "0000123"
-}
+var movimientos = [movimiento1, movimiento2, movimiento3]
 
+var cuentaBancaria1 = new CuentaBancaria("0000123", "Juan Perez", movimientos);
 
-var cuentaBancaria1 = {
-    titular: "Juan Perez",
-    numeroDeCuenta: "0000123",
-    movimientos: [movimiento1, movimiento2]
-}
+console.log(cuentaBancaria1);
+cuentaBancaria1.otroMetodo("123");
+cuentaBancaria1.metodoSinRetorno()
